e3f6c3f5643b9403f58c71fab67fe241aaf004a8
OpenSnake <158470521+OpenSnake@users.noreply.github.com>
2025-06-01 11:39:25 +0300

    Delete tests/anotherPlaceholder.tests.js

--- tests/anotherPlaceholder.tests.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { assert } from 'chai';
-
-describe('Another Placeholder Suite', function () {
-  it('checks that true is true', function () {
-    assert.isTrue(true, 'The expectation is that true is true');
-  });
-});
