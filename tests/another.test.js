d64fb49bbe8ee0669d31ce09c2316ae4cf4d2159
OpenSnake <158470521+OpenSnake@users.noreply.github.com>
2025-06-01 11:47:25 +0300

    Delete tests/another.test.js

--- tests/another.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-
-describe('String operations', () => {
-  it('should concatenate strings', () => {
-    const str = 'Hello, ' + 'world!';
-    assert.strictEqual(str, 'Hello, world!');
-  });
-});
